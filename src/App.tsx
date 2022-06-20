a89e9affa980fe7e8ecc2b80c20d337138b02b56
TayebBou <tayebbousfiha72@gmail.com>
2022-06-20 10:17:09 +0200

    Add Home Page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import styles from './App.module.css';
+import ball from './assets/tennis.png'
 
 const App:FC =() => {
+
+  const [players, setPlayers] = useState([])
+
   return (
-    <div className="App">
+    <>
+    <div className={styles.wallpaper} >
     </div>
+    <img className={styles.ball} src={ball} alt="tennis ball" />
+    <h1 className={styles.h1}>Players Statistics</h1>
+    {
+    }
+    </>
   );
 }
 
